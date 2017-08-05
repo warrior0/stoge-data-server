6a167b501a8e1d19e08cca45f1d048e7ba0e0541
warrior0 <toannguyen035@gmail.com>
2017-08-05 15:45:23 -0700

    create server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var socket = require('socket.io');
+var socketio = require('socket.io');
 var ip = require('ip');
 
 var app = http.createServer(function (req, res) {                   // tạo server và gủi file index.txt
@@ -10,27 +10,31 @@ var app = http.createServer(function (req, res) {                   // tạo ser
     	res.end();
     });
 })
-var oi = socket(app);                                                //tạo đối tượng web socket
+var io = socketio(app);                                                //tạo đối tượng web socket
 app.listen(8080);                                                    // cho server lắng nghe ở cổng 8080
 
 console.log('listenning at port 8080');
-console.log('server run at '+ip.address()+'ip address');             // in địa chỉ ip của server
+console.log('server run at '+ip.address()+' ip address');             // in địa chỉ ip của server
 
-var webapp = io.of('/webapp')
-var esp8266 = io.of('/esp8266')
-var middleware = require('socketio-wildcard')();
-webapp.use(middleware);
-esp8266.use(middleware);
+// var webapp = io.of('/webapp')
+// var esp8266 = io.of('/esp8266')
+// var middleware = require('socketio-wildcard')();
+// webapp.use(middleware);
+// esp8266.use(middleware);
 
-esp8266.on('connection', function(socket){
+io.on('connection', function(socket){
 	console.log('esp8266 connented');
+	
 	socket.on('disconnet', function(){
 		console.log('esp8266 disconneted!');
 	});
-	socket.on('*')
+	socket.on('received_data', function(packet){
+		console.log('esp8266 send data from home');
+		console.log(packet);                                            // đọc chuổi json được gửi từ esp8266
+	});
 });
-fs.appendFile('index.txt', '\n<p>' +'next in line'+ '</p>', function(err){  // ghi dữ liệu vào file index.txt
-	if (err) throw err;
-	console.log('saved');
-});
+// fs.appendFile('index.txt', '\n<p>' +'next in line'+ '</p>', function(err){  // ghi dữ liệu vào file index.txt
+// 	if (err) throw err;
+// 	console.log('saved');
+// });
 
